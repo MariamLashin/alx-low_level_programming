cabe707a8ccad334cc886871ddef7ca88f7bfeeb
MariamLashin <mariam.m.lashin@gmail.com>
2023-06-19 19:37:48 -0700

    rewe

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Enty point
+ *
+ * Description: prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		printf("%i", digit);
+		digit++;
+	}
+	printf("\n");
+
+	return (0);
+
+}
