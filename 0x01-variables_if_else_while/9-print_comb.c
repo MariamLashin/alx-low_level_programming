bbcd338419bb66fedbf07e3c8631c042bf679174
MariamLashin <mariam.m.lashin@gmail.com>
2023-09-12 07:08:19 -0700

    low

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description : print single-digit numbers separated by , .
+ * return : 0 (success)
+ */
+
+int main(void)
+{
+	int num = 0;
+	while (num <= 9)
+	{
+		putchar (num + 48);
+		if (num != 9)
+		{
+			putchar (',');
+			putchar (' ');
+		};
+		num++;
+	}
+	putchar ('\n');
+}
