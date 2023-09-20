d06adcc084d5a948451bd76b1bf6a528ba1049ed
MariamLashin <mariam.m.lashin@gmail.com>
2023-09-19 19:41:44 -0700

    m

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen : retuns the lenght of a string
+ *
+ * return the lenght of a string
+ */
+
+int _strlen(char *s)
+{
+	int counter;
+
+	for(counter = 0; *s != '\0'; s++)
+		counter++;
+
+	return(counter);
+}
