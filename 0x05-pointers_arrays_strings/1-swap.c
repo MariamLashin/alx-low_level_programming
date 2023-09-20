08ed229844f547d666938b4afcdf42ceb4e8d92b
MariamLashin <mariam.m.lashin@gmail.com>
2023-09-19 18:24:32 -0700

    m

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,13 +3,14 @@
 
 /**
  * main - check the code
- *
+ * Description : function swaps the values of two integers a,b
  * Return: nothing
 */
 
 void swap_int(int *a, int *b)
 {
 	int temp;
+
 	temp = *a;
 	*a = *b;
 	*b = temp;
