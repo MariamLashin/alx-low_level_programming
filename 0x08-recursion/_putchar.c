2080f19e3fa9d113675716ea68da5a0fd12a79e8
MariamLashin <mariam.m.lashin@gmail.com>
2023-11-15 18:24:51 -0800

    a

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,12 @@
+#nclude<unistd.h>
+
+/**
+ * _putchar : wrie the char c to stdout
+ * &c : the char to print
+ * return : on success "1" on error "-1"
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
